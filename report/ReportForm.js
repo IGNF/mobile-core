13a86ee2a2fe059b7393087d4c2418f90e1f2620
Madeline Jacopucci <madeline.jacopucci@ign.fr>
2023-06-08 17:27:53 +0200

    bug ios application json

--- report/ReportForm.js
+++ report/ReportForm.js
@@ -1082,7 +1082,11 @@ Report.prototype.postLocalRep = function(georem, georep, options) {
     "content": georep.content
   };
   
-  this.apiClient.addReply(georem.id, body, "application/x-www-form-urlencoded").then((replyResponse) => {
+  let contentType = null;
+    if (CordovApp.getPlatformId() === 'ios') {
+      contentType = "application/x-www-form-urlencoded";
+    }
+  this.apiClient.addReply(georem.id, body, contentType).then((replyResponse) => {
     this.apiClient.getReport(georem.id).then((reportResponse) => {
       var grem =  reportResponse.data;
       this.delLocalRep(georem, georep, {
