17d6e24090274f100b4fc1a705ead8be4785c579
Madeline Jacopucci <madeline.jacopucci@ign.fr>
2024-11-14 14:51:40 +0100

    fix: #19641: symboles non charges

--- ol/style/Collaboratif.js
+++ ol/style/Collaboratif.js
@@ -399,8 +399,10 @@ ol_style_Collaboratif.getUrls = function(featureType) {
   for (let i in featureType.styles) {
     if (featureType.style.externalGraphic) {
       uris[featureType.styles[i].externalGraphic] = featureType.styles[i].uri
-      for (let j in featureType.styles[i].children) {
-        let child = featureType.styles[i].children[j];
+    }
+    for (let j in featureType.styles[i].children) {
+      let child = featureType.styles[i].children[j];
+      if (child.externalGraphic) {
         uris[child.externalGraphic] = child.uri;
       }
     }
